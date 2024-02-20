e1368d21458d1bbf30dfbda9575bec44ef260893
farhan12121 <ashrafislam412@gmail.com>
2024-02-21 00:27:02 +0600

    js function element

--- js/script2.js
+++ js/script2.js
@@ -1,4 +1,30 @@
-function changeBackground(elementId){
+function textSet(id, value){
+    document.getElementById(id).innerText = value;
+}
+function setPriceSection(seatNumber){
+    const setTBody = document.getElementById('t-body');
+    const tList = document.createElement('tr');
+
+    const seatNum = document.createElement('td');
+    const seatClass = document.createElement('td');
+    const seatPrice = document.createElement('td');
+
+    SeatNum.classList.add('py-4' , 't-Data');
+    seatPrice.classList.add('text-right');
+    SeatNum.innerText = seatNumber;
+    seatClass.innerText = 'Economy';
+    seatPrice.innerText = '550';
+
+    tList.appendChild(seatNum);
+    tList.appendChild(seatClass);
+    tList.appendChild(seatPrice);
+    setTBody.appendChild(tList)
+
+}
+
+function elementValue(elementId){
     const element = document.getElementById(elementId);
-    element.classList.add('bg-[#1DD100');
+    const elementText = element.innerText;
+    const elementValue = parseFloat(elementText);
+    return elementValue;
 }
